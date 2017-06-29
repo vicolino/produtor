f5d70982e1378837b46c8ea0ee2dc61eb3ebd0bc
Vinicius Soares <viniciuss@dbserver.com.br>
2017-06-29 19:17:03 -0300

    AJustes Eureka

--- src/main/java/br/com/vj/Application.java
+++ src/main/java/br/com/vj/Application.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @RestController
 public class Application {
 

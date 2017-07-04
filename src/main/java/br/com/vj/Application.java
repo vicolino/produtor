d5b3b9312d7871ae432db26943c1d85ee76030f8
Vinicius Soares <viniciusduranti@gmail.com>
2017-07-04 11:29:49 -0300

    Novo endpoint

--- src/main/java/br/com/vj/Application.java
+++ src/main/java/br/com/vj/Application.java
@@ -18,7 +18,7 @@ public class Application {
     return "{\"value\":"+counter+"}";
   }
 	
-  @RequestMapping(value = "/", reduces = "application/json")
+  @RequestMapping(value = "/reduce", produces = "application/json")
   public String reduces() {
     counter--;
     return "{\"value\":"+counter+"}";
